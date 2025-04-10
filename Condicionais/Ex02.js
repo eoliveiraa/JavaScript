f7d6b382fd16c51a48bfa4379d93e6e8a150dfbf
Emilly Oliveira <emilly.o.santos7@aluno.senai.br>
2025-04-10 13:42:48 -0300

    Criando Exercicios

---
+++ Condicionais/Ex02.js
@@ -0,0 +1,16 @@
+//Verificar se o usuário pode tirar carteira de motorista
+
+let Idade = 17;
+
+if (Idade >= 18) 
+{
+    console.log("Voce pode tirar a carteira🚗")
+}
+else if(Idade < 18)
+{
+console.log("Voce nao pode tirar a carteira😒")
+}
+else
+{
+console.log("Essa informaçao nao eh valida!🤔")
+}
